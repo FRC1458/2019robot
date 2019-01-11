2ffe0edf02ea01d7dd13d307f572563bb8834ff2
NickFromNationwide <ndp1234567890@gmail.com>
2019-01-10 16:18:27 -0800

    Added Code

---
+++ src/main/java/frc/team1458/robot/OI.kt
@@ -0,0 +1,13 @@
+package frc.team1458.robot
+
+import frc.team1458.lib.input.FlightStick
+
+class OI {
+    val leftStick : FlightStick = FlightStick.flightStick(1)
+    val rightStick : FlightStick = FlightStick.flightStick(0)
+
+    var steerAxis = leftStick.rollAxis.scale(0.5) //controlBoard.xbox.rightX.scale(0.35) //
+    var throttleAxis = rightStick.pitchAxis.inverted
+
+    var slowDownButton = rightStick.trigger
+}
--- src/main/java/frc/team1458/robot/Robot.kt
+++ src/main/java/frc/team1458/robot/Robot.kt
@@ -1,20 +1,55 @@
 package frc.team1458.robot
 
-import edu.wpi.first.wpilibj.TimedRobot
-import edu.wpi.first.wpilibj.smartdashboard.SendableChooser
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard
+import frc.team1458.lib.actuator.SmartMotor
+import frc.team1458.lib.actuator.Solenoid
 import frc.team1458.lib.util.flow.delay
 import frc.team1458.lib.core.BaseRobot
+import frc.team1458.lib.drive.TankDrive
+import frc.team1458.lib.pid.PIDConstants
 
 
 class Robot : BaseRobot() {
 
+    val oi: OI = OI()
+    val dt: TankDrive = TankDrive(
+        leftMaster = SmartMotor.CANtalonSRX(16),
+        rightMaster = SmartMotor.CANtalonSRX(22).inverted,
+        leftMotors = arrayOf(SmartMotor.CANtalonSRX(25)),
+        rightMotors = arrayOf(SmartMotor.CANtalonSRX(23).inverted),
+        closedLoopControl = true,
+        wheelDiameter = 0.5,
+        closedLoopScaling = 6.0, // TODO: determine
+
+        pidConstantsLowGearLeft =  PIDConstants(0.5, kI = 0.001, kD = 0.05, kF = 1.0/1798.8), // These are decent
+        pidConstantsLowGearRight = PIDConstants(0.5, kI = 0.001, kD = 0.05, kF = 1.0/1806.4), // These are decent2
+
+        shifter = Solenoid.doubleSolenoid(extendChannel = 1, retractChannel = 0)
+                + Solenoid.doubleSolenoid(extendChannel = 2, retractChannel = 3),
+
+        pidConstantsHighGearLeft = PIDConstants(0.15, kI = 0.001, kD = 0.01, kF = 1.0/6530.5), // TODO: determine
+        pidConstantsHighGearRight = PIDConstants(0.15, kI = 0.001, kD = 0.01, kF = 1.0/6877.7), // TODO: determine
+        autoShift = false,
+        shiftUpSpeed = 10.0, // TODO: determine
+        shiftDownSpeed = 12.0, // TODO: determine
+        shiftCooldown = 3.0,
+
+        accelLimit = 1000000.0 // feet/sec^2
+    )
+
+    val drivetrainInverted: Boolean = false
+
     override fun robotSetup() {
-        println("Hello World!")
+        println("Setup")
+
+        dt.leftMaster.connectedEncoder.zero()
+        dt.rightMaster.connectedEncoder.zero()
     }
 
     override fun runAuto() {
         println("Warning: Sandstorm")
+        dt.tankDrive(0.4, 0.1)
+        delay(400)
+        dt.tankDrive(0.4, 0.0)
     }
 
     override fun teleopInit() {
@@ -23,6 +58,20 @@ class Robot : BaseRobot() {
 
     override fun teleopPeriodic() {
         // drive code - runs around 50hz
+        dt.arcadeDrive(
+            if (drivetrainInverted) {
+                -0.5 * (oi.throttleAxis.value)
+            } else if (oi.slowDownButton.triggered) {
+                0.5 * oi.throttleAxis.value
+            } else {
+                oi.throttleAxis.value
+            },
+            if (drivetrainInverted) {
+                (oi.steerAxis.value)
+            } else {
+                oi.steerAxis.value
+            }
+        )
     }
 
     override fun runTest() {
